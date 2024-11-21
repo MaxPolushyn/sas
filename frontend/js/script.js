c1ee374977a8bc5edf20f3b8a510b0d88ed1637a
MaxPolushyn <polusinmaksim42@gmail.com>
2024-11-21 12:02:15 +0200

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -14,7 +14,7 @@ document.getElementById('registrationForm').addEventListener('submit', async fun
     }
 
     try {
-        const response = await fetch('http://localhost:5000/api/auth/register', {
+        const response = await fetch('/api/auth/register', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ email, firstName, lastName, password, role })
